732f09c32799bf01bd1b6bff201885630744984a
Kateryna Tytova <titova.kat.85@gmail.com>
2020-10-13 10:50:42 +0300

    feat: add solution brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = {};
+  for (item of bracketsConfig) {
+    for (let i = 0; i<item.length; i++) {
+      brackets[item[i]] = item[i+1];
+      i++;
+    }
+  }
+  str = str.split('');
+  let openBrackets = [],
+  flagSimpleBrackets = 0;
+  for (item of str){ 
+    if (openBrackets.includes(brackets[item])) flagSimpleBrackets = 1;
+    if (brackets[item] && flagSimpleBrackets == 0) {
+      openBrackets.push(item);
+    }
+    else 
+      if (item != brackets[openBrackets.pop()]) 
+        return false;
+      else flagSimpleBrackets = 0;
+  }
+  if (openBrackets.length != 0) return false;
+  return true;
 }
